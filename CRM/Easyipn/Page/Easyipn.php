32a9bbd1538255d31bd7781a6a9c5da19518b637
lucky091588 <lucky091588@gmail.com>
2020-07-22 07:13:40 +0800

    civilint file

--- CRM/Easyipn/Page/Easyipn.php
+++ CRM/Easyipn/Page/Easyipn.php
@@ -7,7 +7,7 @@ class CRM_Easyipn_Page_Easyipn extends CRM_Core_Page {
     // Example: Set the page-title dynamically; alternatively, declare a static title in xml/Menu/*.xml
     CRM_Utils_System::setTitle(E::ts('EasyIPN'));
 
-    if(isset($_GET['payment_processor_id'])) {
+    if (isset($_GET['payment_processor_id'])) {
       $ipn_full_link = CRM_Utils_System::url('civicrm/payment/ipn/' . $_GET['payment_processor_id'], NULL, TRUE, NULL, FALSE, TRUE);
       $this->assign('ipn_link', $ipn_full_link);
     }
@@ -18,4 +18,5 @@ class CRM_Easyipn_Page_Easyipn extends CRM_Core_Page {
   public function get_ipn_page_url($query = NULL) {
     return CRM_Utils_System::url('civicrm/admin/setting/ipn-url', $query);
   }
+
 }
